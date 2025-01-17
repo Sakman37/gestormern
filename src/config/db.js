2e2b19bdd6f85d5dc09268a757301fb8b9985705
Sakman37 <juanpablo201460@gmai.com>
2025-01-17 17:37:38 -0500

    Actualizacion atlas

--- src/config/db.js
+++ src/config/db.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://127.0.0.1:5006/gestormern");
-
+        await mongoose.connect("mongodb+srv://juanpablo201460:ty51fDQ4CHKXdmtQ@gestormern.9f1ge.mongodb.net/gestormern?retryWrites=true&w=majority");
         console.log('✅ MongoDB Conectado');
     } catch (error) {
         console.error('❌ Error en la conexión a MongoDB:', error);
